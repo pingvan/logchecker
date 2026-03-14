fd19b988fce0bed259cbefcaf37ff5ccd9319fbe
Egor Albov <albov.egor@gmail.com>
2026-03-14 18:52:36 +0300

    create Rule interface and rules registry (empty for stage 1)

---
+++ internal/rules/registry.go
@@ -0,0 +1,3 @@
+package rules
+
+var AllRules = []Rule{}
---
+++ internal/rules/rule.go
@@ -0,0 +1,12 @@
+package rules
+
+import (
+	"go/ast"
+
+	"golang.org/x/tools/go/analysis"
+)
+
+type Rule interface {
+	Name() string
+	CheckRule(pass *analysis.Pass, call *ast.CallExpr)
+}
