91d48d489486e45168250808ceb65c44529bb5cb
Egor Albov <albov.egor@gmail.com>
2026-03-14 20:51:11 +0300

    fix lowercase rules

--- testdata/src/lowercase/lowercase.go
+++ testdata/src/lowercase/lowercase.go
@@ -11,11 +11,11 @@ func main() {
 
 	slog.Info("starting server")          // ok
 	slog.Error("failed to connect")       // ok
-	slog.Info("Starting server on port")  // want `log message should start with an lowercase letter`
-	slog.Error("Failed to connect to db") // want `log message should start with an lowercase letter`
+	slog.Info("Starting server on port")  // want `log message must start with lowercase`
+	slog.Error("Failed to connect to db") // want `log message must start with lowercase`
 
 	logger.Info("starting server")    // ok
 	logger.Error("failed to connect") // ok
-	logger.Info("Starting server")    // want `log message should start with an lowercase letter`
-	logger.Error("Failed to connect") // want `log message should start with an lowercase letter`
+	logger.Info("Starting server")    // want `log message must start with lowercase`
+	logger.Error("Failed to connect") // want `log message must start with lowercase`
 }
